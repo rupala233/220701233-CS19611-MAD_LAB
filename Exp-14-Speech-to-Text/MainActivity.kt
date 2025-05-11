f729526385cb16af44d4800e3c12b143fcd6cefb
rupala233 <220701233@rajalakshmi.edu.in>
2025-05-11 19:33:58 +0530

    Create MainActivity.kt

---
+++ Exp-14-Speech-to-Text/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+import android.app.ComponentCaller
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btSpeak: Button
+    lateinit var tvText:TextView
+    private val REQUEST_CODE_SPEECH_INPUT=100
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        tvText=findViewById(R.id.tvText)
+        btSpeak=findViewById(R.id.Speak)
+        btSpeak.setOnClickListener {
+            val intent= Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"SPEAK SOMETHING")
+            startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        caller: ComponentCaller
+    ) {
+        super.onActivityResult(requestCode, resultCode, data, caller)
+        if(requestCode==REQUEST_CODE_SPEECH_INPUT && resultCode==RESULT_OK){
+            val result=data?.getStringArrayExtra(RecognizerIntent.EXTRA_RESULTS)
+            tvText.text=result?.get(0)
+        }
+    }
+}
